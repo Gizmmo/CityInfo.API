f22f3de2f722c5f7bc3fde09901200069ecf148d
Travis Scott <travisscott301@gmail.com>
2019-09-23 00:44:26 -0500

    added context to get city

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace CityInfo.API
 #else
             services.AddTransient<IMailService, CloudMailService>();
 #endif
+            services.AddScoped<ICityInfoRepository, CityInfoRepository>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
